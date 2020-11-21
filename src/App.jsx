68e5824459efcece83130e1a4773b83e6cd97180
Daniel Singer <dannys430@yahoo.com>
2020-11-20 21:16:03 -0500

    converted class component to functional component with hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react'
 import mapboxgl from "mapbox-gl";
 import {mapbox} from "./keys";
 
-mapboxgl.accessToken = mapbox.token;
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    
-    this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 2
-    }
-  }
+export const App = () => {
+  const [getCoords, setCoords] = useState([-0.11, 51.5])
   
-  componentDidMount() {
+  
+  const node = useRef(null);
+  
+  useEffect(() => {
+    mapboxgl.accessToken = mapbox.token;
+
+    navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
     const map = new mapboxgl.Map({
-      container: this.mapContainer,
+      container: node.current,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
-    })
-    return map
+      center: getCoords,
+      zoom: 5
+    }) 
+  }, [map])
+  
+  
+  const successLocation = (position) => {
+    // console.log(position)
+    setCoords([position.coords.longitude, position.coords.latitude])
   }
   
-  render() {
+  const errorLocation = () => {}
+  
 
-    return (
-      <div>
-        <div ref={el => this.mapContainer = el} className="mapContainer"/>
-      </div>
-    )
-  }
+  return(
+    <div>
+      <div ref={node} className="mapContainer"></div>
+    </div>
+  )
+
 
 
 }
-
-
-export default App;
