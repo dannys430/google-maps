695db75602cd019beceb74f6b7479d8d7c2e0e81
Daniel Singer <dannys430@yahoo.com>
2020-11-23 23:37:06 -0500

    updated mpa

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useRef} from 'react'
 import mapboxgl from "mapbox-gl";
 import {mapbox} from "./keys";
 
+mapboxgl.accessToken = mapbox.token;
 
 export const App = () => {
   const [getCoords, setCoords] = useState([-0.11, 51.5])
@@ -10,32 +11,38 @@ export const App = () => {
   const node = useRef(null);
   
   useEffect(() => {
-    mapboxgl.accessToken = mapbox.token;
-
-    navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
+    
     const map = new mapboxgl.Map({
       container: node.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: getCoords,
       zoom: 5
     }) 
-  }, [map])
+    
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+    
+    navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
+
+    return () => map.remove()
+  }, [])
   
-  
-  const successLocation = (position) => {
-    // console.log(position)
+  const successLocation = (position) => {      
     setCoords([position.coords.longitude, position.coords.latitude])
+    map.center = getCoords
   }
   
   const errorLocation = () => {}
   
-
+  
+  
+  
+  
   return(
     <div>
       <div ref={node} className="mapContainer"></div>
     </div>
   )
-
+  
 
 
 }
